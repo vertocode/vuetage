8af8163807a6fb4810d24204d76104c133c3557a
vertocode <evertonvanoni1@gmail.com>
2023-11-04 20:53:43 -0300

    fix: import style.css in result of vite build

--- dist/README.md
+++
@@ -1,2 +0,0 @@
-# vuetage lib
-Vue.js components
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,13 @@
+set -e
 vue-tsc --noEmit && vite build
+
+# For some reason the build of vite is not importing automatically the style.css, this is a treatment to import css correctly.
+# TODO: Investigate a better way to do it and remove these lines.
+sed -i '' -e 's/from "vue";/from "vue"; import style from/g' dist/vuetage.js
+sed -i '' -e 's/style from/style from "style.css"/g' dist/vuetage.js
+sed -i '' -e 's/"style.css"/".\/style.css"/g' dist/vuetage.js
+sed -i '' -e 's/import style/\nimport style/g' dist/vuetage.js
+
 cp package.json dist
 cp README.md dist
 cp LICENSE dist
