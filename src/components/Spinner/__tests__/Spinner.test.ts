15bc0b01d547cf17f2ae2323ff49a816c08cd401
vertocode <evertonvanoni1@gmail.com>
2023-11-14 18:44:56 -0300

    feat: create spinner snap test

---
+++ src/components/Spinner/__tests__/Spinner.test.ts
@@ -0,0 +1,10 @@
+// @vitest-environment happy-dom
+
+import { expect, it } from 'vitest'
+import { Spinner } from '@/components'
+import { mount } from '@vue/test-utils'
+
+it('should match content snapshot using default template', () => {
+    const wrapper = mount(Spinner)
+    expect(wrapper.html()).toMatchSnapshot('__snapshots__/Spinner.snap')
+})
