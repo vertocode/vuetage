bc5f1740f82b568bf23a1f56731db1b04fc0c77e
vertocode <evertonvanoni1@gmail.com>
2023-12-06 19:48:14 -0300

    feat: create default story for base text field

---
+++ src/components/Input/__stories__/BaseTextField.stories.tsx
@@ -0,0 +1,27 @@
+import type  { Meta, StoryObj } from '@storybook/vue3'
+import { BaseButton, BaseTextField } from '@/components'
+
+const meta: Meta = {
+    title: 'Input/BaseTextField',
+    component: BaseTextField,
+    tags: ['input', 'textField'],
+    argTypes: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    render: (args) => ({
+        components: { BaseTextField },
+        setup() {
+            return { args }
+        },
+        template: '<BaseTextField v-bind="args">Default</BaseTextField>'
+    }),
+    args: {
+        ...BaseButton.args
+    }
+}
+
+Default.args = {}
--- src/components/main.ts
+++ src/components/main.ts
@@ -1,8 +1,9 @@
 import type { App } from 'vue'
-import { Spinner, BaseButton } from '@/components'
+import { Spinner, BaseButton, BaseTextField } from '@/components'
 export default {
 	install: (app: App) => {
 		app.component('BaseButton', BaseButton)
+		app.component('BaseTextField', BaseTextField)
 		app.component('Spinner', Spinner)
 	}
 }
