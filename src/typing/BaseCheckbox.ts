f89489b69cf4587fab105de190dff4bf515ad3e5
vertocode <evertonvanoni1@gmail.com>
2024-06-02 18:52:21 -0300

    fix: type

--- src/typing/BaseCheckbox.ts
+++ src/typing/BaseCheckbox.ts
@@ -3,7 +3,7 @@ import { Color } from '@/typing/Color'
 
 export interface Props {
     modelValue: boolean
-    color?: Color | string
+    color?: Color
     inputStyle?: CustomStyle
     key?: string
     defaultChecked?: boolean
